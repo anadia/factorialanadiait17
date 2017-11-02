e4e04a7639c8eeb4146835035d5e035b636c2ae8
oscarterres <oscarterres@anadia.es>
2017-11-02 17:49:33 +0100

    Ultimo cambio estilo Google

--- src/main/java/org/anadiait/factorial/Factorial.java
+++ src/main/java/org/anadiait/factorial/Factorial.java
@@ -1,18 +1,31 @@
 package org.anadiait.factorial;
 
+/**
+ * Clase que computa el factorial de un long
+ * 
+ * @author Oscar Terres
+ *
+ */
+
 public class Factorial {
 
-	public long compute(long value) {
-	    if((value == 0) || (value ==1)) {
-	    	return 1;
-	    	
-	    }else if (value == 2) {
-	    	return 2; 
-	    	
-	    }else if  (value == 2) {
-	    	return 6;	
-	    }else 
-		return value = compute (value -1); 
-	}
+  public long compute(long value) {
+    long result;
+    if (value < 0) {
+      throw new RuntimeException("The value is negative: " + value);
+    }
+    if ((value == 0) || (value == 1)) {
+      result = 1;
+    } else {
+      result = value * compute(value - 1);
+    }
+    return result;
+  }
+
+  public String toString() {
+    int a = 5;
+    long b = a * 5;
+    return "";
+  }
 
 }
