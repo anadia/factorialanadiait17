b5be0f263ee02ecafb87454e8a38c9eaaa490d1b
oscarterres <oscarterres@anadia.es>
2017-11-02 17:59:48 +0100

    Google style

--- src/test/java/org/anadiait/factorial/FactorialTest.java
+++ src/test/java/org/anadiait/factorial/FactorialTest.java
@@ -1,51 +1,50 @@
 package org.anadiait.factorial;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
-public class FactorialTest { 
-    @Test
-    public void test1() {
-    	Factorial factorial = new Factorial() ;
-    	
-    	long result = factorial.compute(0) ;
-    	long expectedValue =1 ;
-    	
-    	assertEquals(expectedValue, result );
-    }
-    @Test
-    public void test2() {
-    	Factorial factorial = new Factorial() ; 
-    	
-    	long result = factorial.compute(1) ;
-    	long expectedValue =2 ;
-    	
-    	assertEquals(expectedValue, result );
-    }    
-    @Test
-    public void test3() {
-    	Factorial factorial = new Factorial() ;
-    	
-    	long result = factorial.compute(2) ;
-    	long expectedValue =6 ;
-    	
-    	assertEquals(expectedValue, result );
-    }
-    @Test
-    public void test5() {
-    	Factorial factorial = new Factorial() ;
-    	
-    	long result = factorial.compute(3) ;
-    	long expectedValue =120 ;
-    	
-    	assertEquals(expectedValue, result );
-    }
-    
-    
+public class FactorialTest {
+  @Test
+  public void test1() {
+    Factorial factorial = new Factorial();
+
+    long result = factorial.compute(0);
+    long expectedValue = 1;
+
+    assertEquals(expectedValue, result);
+  }
+
+  @Test
+  public void test2() {
+    Factorial factorial = new Factorial();
+
+    long result = factorial.compute(1);
+    long expectedValue = 2;
+
+    assertEquals(expectedValue, result);
+  }
+
+  @Test
+  public void test3() {
+    Factorial factorial = new Factorial();
+
+    long result = factorial.compute(2);
+    long expectedValue = 6;
+
+    assertEquals(expectedValue, result);
+  }
+
+  @Test
+  public void test5() {
+    Factorial factorial = new Factorial();
+
+    long result = factorial.compute(3);
+    long expectedValue = 120;
+
+    assertEquals(expectedValue, result);
+  }
+
+
 }
-
-
-
 
 
