b40dfe667574126065dcdbf2ce2c121a930da480
oscarterres <oscarterres@anadia.es>
2017-11-02 19:19:21 +0100

    Arreglado

--- src/main/java/org/anadiait/factorial/Factorial.java
+++ src/main/java/org/anadiait/factorial/Factorial.java
@@ -9,7 +9,6 @@ package org.anadiait.factorial;
 
 public class Factorial {
 
-<<<<<<< HEAD
 	public long compute(long value) {
 		long result ;
 		if (value < 0) {
@@ -28,25 +27,5 @@ public class Factorial {
 		long b = a * 5;
 		return "";
 	}
-=======
-  public long compute(long value) {
-    long result;
-    if (value < 0) {
-      throw new RuntimeException("The value is negative: " + value);
-    }
-    if ((value == 0) || (value == 1)) {
-      result = 1;
-    } else {
-      result = value * compute(value - 1);
-    }
-    return result;
-  }
-
-  public String toString() {
-    int a = 5;
-    long b = a * 5;
-    return "";
-  }
->>>>>>> master
 
 }
--- src/test/java/org/anadiait/factorial/FactorialTest.java
+++ src/test/java/org/anadiait/factorial/FactorialTest.java
@@ -1,7 +1,6 @@
 package org.anadiait.factorial;
 
 import static org.junit.Assert.assertEquals;
-<<<<<<< HEAD
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,53 +33,6 @@ public class FactorialTest {
 
 	@Test
 	public void shouldFactorialOfTwoReturnTwo() {
-=======
-import org.junit.Test;
-
-public class FactorialTest {
-  @Test
-  public void test1() {
-    Factorial factorial = new Factorial();
-
-    long result = factorial.compute(0);
-    long expectedValue = 1;
-
-    assertEquals(expectedValue, result);
-  }
-
-  @Test
-  public void test2() {
-    Factorial factorial = new Factorial();
-
-    long result = factorial.compute(1);
-    long expectedValue = 2;
-
-    assertEquals(expectedValue, result);
-  }
-
-  @Test
-  public void test3() {
-    Factorial factorial = new Factorial();
-
-    long result = factorial.compute(2);
-    long expectedValue = 6;
-
-    assertEquals(expectedValue, result);
-  }
-
-  @Test
-  public void test5() {
-    Factorial factorial = new Factorial();
-
-    long result = factorial.compute(3);
-    long expectedValue = 120;
-
-    assertEquals(expectedValue, result);
-  }
-
-
-}
->>>>>>> master
 
 		long result = factorial.compute(2);
 		long expectedValue = 2;
